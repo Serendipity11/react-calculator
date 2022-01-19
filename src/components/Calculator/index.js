09ba7f8419c403707b2cee006fb324ab650c2625
V S <66644986+Serendipity11@users.noreply.github.com>
2022-01-19 11:10:36 -0500

    Added numbers to the buttons

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -11,20 +11,20 @@ export default function Calculator() {
       <Button gridSpan={2}>AC</Button>
       <Button control>DEL</Button>
       <Button operation>÷</Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
+      <Button>7</Button>
+      <Button>8</Button>
+      <Button>9</Button>
       <Button operation>x</Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
+      <Button>4</Button>
+      <Button>5</Button>
+      <Button>6</Button>
       <Button operation>+</Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
+      <Button>1</Button>
+      <Button>2</Button>
+      <Button>3</Button>
       <Button operation>-</Button>
       <Button period>.</Button>
-      <Button></Button>
+      <Button>0</Button>
       <Button gridSpan={2} equals>
         =
       </Button>
